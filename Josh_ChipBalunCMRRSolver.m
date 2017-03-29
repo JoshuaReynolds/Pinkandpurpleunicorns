ba6a642652b19e70e4ab4388255a56275367b165
JoshuaReynolds <joshua.reynolds@ou.edu>
2017-03-29 11:34:53 -0500

    Instructions
    Added instructions for using this solver.

--- Josh_ChipBalunCMRRSolver.m
+++ Josh_ChipBalunCMRRSolver.m
@@ -1,4 +1,18 @@
-MeasuredData = read(rfdata.data,'JTX-2.s3p'); % Reading in S-parameter data from PNA file
+%%%%%%%%%%%%%%%%%%%
+% Joshua Reynolds %
+% CMRR Solver     %
+% 3-29-2017       %
+%%%%%%%%%%%%%%%%%%%
+
+% Instructions for use: Ensure this file is in the path you are currently
+% using. Input the correct file name in the read command for MeasuredData.
+% Example: 'JTX-2.s3p'
+%
+% WARNING: If your file name contains '.' anywhere, this solver will not be
+% able to recognize the file type and will give you errors. Rename your
+% file to exclude using '.' and the problem should be corrected.
+
+MeasuredData = read(rfdata.data,''); % Reading in S-parameter data from PNA file
 % Measuredf = MeasuredData.freq; % Extracting frequency data from measurement file
 [MeasuredS,freq] = extract(MeasuredData,'S_PARAMETERS'); % Extracting S parameter data from measurement file
 
